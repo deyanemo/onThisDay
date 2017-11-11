742a7e31a5cede0083b75df87bc75746c8cd4c01
Your Name <you@example.com>
2017-11-11 23:42:53 +0100

    First Release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+deyanemo: string;
+name: string;
+last: string;
+storageNameSpace = 'myNgApp';
+data: any;
+result: any;
+status: string = 'Births';
+constructor(private http:HttpClient) {}
+
+
+ngOnInit(){
+  // For offline Use Stopped for now
+  // const save = localStorage.getItem(this.storageNameSpace);
+  // const storage = (localStorage.getItem(this.storageNameSpace) !== null ? this.data = JSON.parse(save) : this.data = []);
+  // API CALL 
+  this.ApiCall();
+}
+
+
+
+ApiCall(){
+    // API CALL
+    this.http.get('http://history.muffinlabs.com/date').subscribe(data => {
+      // data is now an instance of type ItemsResponse, so you can do this:
+      this.data = data.data.Births;
+      return this.result = data;
+    });
+}
+
+birth() {
+  this.status = 'Births';
+  return this.data = this.result.data.Births;
+}
+death() {
+  this.status = 'Deaths';
+  return this.data = this.result.data.Deaths;
+}
+event() {
+  this.status = 'Events';
+  return this.data = this.result.data.Events;
+}
+
+
+
+
+addData(e){
+  return this.data.push(e);
+}
+storeData() {
+  return localStorage.setItem(this.storageNameSpace, JSON.stringify(this.data));
+}
+
+theMan(){
+  if (this.name !== 'undefined' && this.last !== 'undefined') {
+    this.addData({name: this.name , last: this.last});
+    this.storeData();
+  }
+
 }
